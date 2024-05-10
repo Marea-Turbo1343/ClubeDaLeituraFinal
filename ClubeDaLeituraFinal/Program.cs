f3e80d1599d5b73919873c19d79d734c8cff9da8
Thiago Culau <thiago.culau1@outlook.com>
2024-05-10 15:53:19 -0300

    Add project files.

---
+++ ClubeDaLeituraFinal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClubeDaLeituraFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
